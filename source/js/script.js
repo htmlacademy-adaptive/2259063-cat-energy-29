a37e08ed24a55f826edfe3f772c13359f869987a
do6roetyro <do6roeytro@yandex.ru>
2023-08-12 18:41:30 +0300

    Адаптив catalog. Коммит №1

--- source/js/script.js
+++ source/js/script.js
@@ -3,5 +3,11 @@ let button = document.querySelector(".main-nav__toggle");
 
 console.log(menu);
 
+// button.addEventListener("click", handler);
 
-
+button.onclick = function () {
+  if (menu.classList.contains("list main-nav__list--close")) {
+    menu.classList.remove("main-nav__list--close");
+  }
+  else { menu.classList.add("main-nav__list--close"); }
+}
