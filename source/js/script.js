917b50d4fed3824621056cbb84fcfef9b7284170
do6roetyro <do6roeytro@yandex.ru>
2023-08-20 10:55:10 +0300

    Адаптив. Доработки

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,6 @@
 let menu = document.querySelector(".main-nav__list");
 let button = document.querySelector(".main-nav__toggle");
 
-
-
 button.onclick = function () {
   menu.classList.toggle('main-nav__list--close');
   button.classList.toggle('main-nav__toggle--opened');
