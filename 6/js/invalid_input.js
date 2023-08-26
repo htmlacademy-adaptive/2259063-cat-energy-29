4d893292f6b5afe866d0136d6474d87115795baa
Keks <keks@htmlacademy.ru>
2023-08-26 17:43:22 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/invalid_input.js
@@ -0,0 +1,7 @@
+
+let invalid = function () {
+  if (document.getElementsByClassName('contacts-owner__input')[0].style.borderColor = "#ff8282"
+  ) {
+    document.getElementsByClassName('contacts-owner__label')[0].style.backgroundColor = "#ff8282"
+  }
+}
