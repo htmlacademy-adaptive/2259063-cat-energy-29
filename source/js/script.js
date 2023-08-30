2eefadb99b55d0ee75d6a8a848e8bdbbf1c77617
do6roetyro <do6roeytro@yandex.ru>
2023-08-30 22:12:41 +0300

    nojs iframe

--- source/js/script.js
+++ source/js/script.js
@@ -2,10 +2,11 @@ let menu = document.querySelector(".main-nav__list");
 let button = document.querySelector(".main-nav__toggle");
 let nojs = document.querySelector(".menu-nojs");
 let nav = document.querySelector("nav");
-let  map = document.querySelectorAll("iframe")
+let map = document.querySelector("iframe")
 
 nav.classList.remove("menu-nojs");
-map.classList.remove("contacts__location")
+map.classList.remove("contacts__location-nojs");
+
 
 button.onclick = function () {
   menu.classList.toggle("main-nav__list--close");
