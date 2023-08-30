6451358072bccfa66060b7da6eb9144e754e1b7b
do6roetyro <do6roeytro@yandex.ru>
2023-08-30 21:37:31 +0300

    меню nojs

--- source/js/invalid_input.js
+++
@@ -1,7 +0,0 @@
-
-let invalid = function () {
-  if (document.getElementsByClassName('contacts-owner__input')[0].style.borderColor = "#ff8282"
-  ) {
-    document.getElementsByClassName('contacts-owner__label')[0].style.backgroundColor = "#ff8282"
-  }
-}
--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,13 @@
 let menu = document.querySelector(".main-nav__list");
 let button = document.querySelector(".main-nav__toggle");
+let nojs = document.querySelector(".menu-nojs");
+let nav = document.querySelector("nav");
+let  map = document.querySelectorAll("iframe")
+
+nav.classList.remove("menu-nojs");
+map.classList.remove("contacts__location")
 
 button.onclick = function () {
-  menu.classList.toggle('main-nav__list--close');
-  button.classList.toggle('main-nav__toggle--opened');
+  menu.classList.toggle("main-nav__list--close");
+  button.classList.toggle("main-nav__toggle--opened");
 }
